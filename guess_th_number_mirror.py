a9205a55d6acaf1e845fb6c0fe702aabe90f5f94
Piotr Michna <pm@piotrmichna.pl>
2020-11-20 09:55:59 +0100

    star of definition fun. find_user_number

--- guess_th_number_mirror.py
+++ guess_th_number_mirror.py
@@ -11,3 +11,12 @@ def get_user_answer(dir, number):
         elif result== 'n':
             return False
 
+
+def find_user_number():
+    print('Think and remember the number from 1 ... 100\nand I will try to guess it ;)')
+    max_number=100
+    min_number=1
+    dir=1
+
+    dif_number=int( (max_number-min_number)/2+min_number)
+
